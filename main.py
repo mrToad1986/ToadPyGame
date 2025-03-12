0d22f27292a62e1c9c44c969f8f02a0e2e1bcfc0
mrToad1986 <korostylev.ov@gmail.com>
2025-03-12 12:08:43 +0300

    add background img

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ pygame.init()
 pygame.display.set_caption(const.GAME_TITLE)
 screen = pygame.display.set_mode(const.WIN_SIZE)
 screen.fill(variables.bg_color['серо-зеленый'])
+
+#фоновое изображение
+background_image = pygame.image.load("img/bg1.png")
+background_image = pygame.transform.scale(background_image, const.WIN_SIZE)
+screen.blit(background_image, (0,0))
+
+
 done = False
+# переписать через while true и вынести flip из цикла?
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
